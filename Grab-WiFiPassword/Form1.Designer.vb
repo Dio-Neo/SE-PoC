c234803be326751300c1460f45ca95a4b89371a7
Ice <rubble-ice@outlook.com>
2017-02-04 21:26:00 +0900

    Wireless Attack

---
+++ Grab-WiFiPassword/Form1.Designer.vb
@@ -0,0 +1,75 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.rtbProfile = New System.Windows.Forms.RichTextBox()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.StartToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.MenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'rtbProfile
+        '
+        Me.rtbProfile.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.rtbProfile.Location = New System.Drawing.Point(0, 24)
+        Me.rtbProfile.Name = "rtbProfile"
+        Me.rtbProfile.Size = New System.Drawing.Size(284, 237)
+        Me.rtbProfile.TabIndex = 0
+        Me.rtbProfile.Text = ""
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.StartToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(284, 24)
+        Me.MenuStrip1.TabIndex = 1
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'StartToolStripMenuItem
+        '
+        Me.StartToolStripMenuItem.Name = "StartToolStripMenuItem"
+        Me.StartToolStripMenuItem.Size = New System.Drawing.Size(42, 20)
+        Me.StartToolStripMenuItem.Text = "start"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 261)
+        Me.Controls.Add(Me.rtbProfile)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents rtbProfile As RichTextBox
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents StartToolStripMenuItem As ToolStripMenuItem
+End Class
---
+++ Grab-WiFiPassword/Form1.vb
@@ -0,0 +1,33 @@
+﻿Public Class Form1
+    Const Quote As String = """"
+
+    Sub New()
+
+        ' This call is required by the designer.
+        InitializeComponent()
+
+
+    End Sub
+
+    Private Sub Grab()
+        Dim Proc As New Process()
+        With Proc.StartInfo
+            .FileName = "cmd.exe"
+            .Arguments = "/c for /f " & Quote & "skip=9 tokens=1,2 delims=:" & Quote & " %i in ('netsh wlan show profiles') do @echo %j | findstr -i -v echo | netsh wlan show profiles %j key=clear"
+            .UseShellExecute = False
+            .CreateNoWindow = True
+            .RedirectStandardOutput = True
+            .RedirectStandardError = True
+        End With
+        Proc.Start()
+        While Not Proc.StandardOutput.EndOfStream
+            rtbProfile.AppendText(Proc.StandardOutput.ReadLine() & vbNewLine)
+        End While
+        Proc.WaitForExit()
+    End Sub
+
+    Private Sub StartToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles StartToolStripMenuItem.Click
+        'rtbProfile.AppendText("for /f " & Quote & "skip=9 tokens=1,2 delims=:" & Quote & " %i in ('netsh wlan show profiles') do @echo %j | findstr -i -v echo | netsh wlan show profiles %j key=clear")
+        Grab()
+    End Sub
+End Class
